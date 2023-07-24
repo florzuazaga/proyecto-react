4f7ef2d8aec1e60687b53f2191989d4c14d33278
florencia <florenciazuazaga47@gmail.com>
2023-07-24 16:49:48 -0300

    cambios 2

--- mock/data.js
+++ mock/data.js
@@ -1,23 +1,3 @@
-const films = [
-  {
-    id: 1,
-    category: "",
-    image: "./public/img/descarga.jpg",
-    title: "sarten",
-  },
-  {
-    id: 2,
-    category: "",
-    image: "./public/img/imagenes.jpg",
-    title: "cacerola cuadrada",
-  },
-  {
-    id: 3,
-    category: "",
-    image: "./public/img/Cacerola_28_Cherry.png",
-    title: "cacerola 28cm",
-  },
-];
 export const getProducts = () => {
   return new Promise((resolve, reject) => {
     let error = false;
--- src/componentes/ItemlistContainer/index.jsx
+++ src/componentes/ItemlistContainer/index.jsx
@@ -1,8 +1,27 @@
 import React, { useState, useEffect } from "react";
-
+import ItemCount from "../ItemCount";
 import itemList from "../CartWidget/itemList";
 import Titulo from "../Titulo";
-
+const films = [
+  {
+    id: 1,
+    category: "",
+    image: "./public/img/descarga.jpg",
+    title: "sarten",
+  },
+  {
+    id: 2,
+    category: "",
+    image: "./public/img/imagenes.jpg",
+    title: "cacerola cuadrada",
+  },
+  {
+    id: 3,
+    category: "",
+    image: "./public/img/Cacerola_28_Cherry.png",
+    title: "cacerola 28cm",
+  },
+];
 export const ItemlistContainer = ({ texto }) => {
   const [data, setData] = useState([]);
   useEffect(() => {
