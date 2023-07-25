9a365198f1a6f97a01a86b54a4099b947324f06f
florencia <florenciazuazaga47@gmail.com>
2023-07-25 12:13:47 -0300

    cambiogetdata

--- src/componentes/ItemlistContainer/index.jsx
+++ src/componentes/ItemlistContainer/index.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import ItemList from "../CartWidget/ItemList";
 import Titulo from "../Titulo";
+import { getData } from "../../Data/producto";
 
 export const ItemlistContainer = ({ texto }) => {
   const [data, setData] = useState([]);
