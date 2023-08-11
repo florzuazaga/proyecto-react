863f3b88052ccbe8443ae688c784dcf3f10b99f5
florencia <florenciazuazaga47@gmail.com>
2023-08-11 11:33:51 -0300

    cambiocontador

--- src/componentes/Titulo/CartWidget.jsx
+++ src/componentes/Titulo/CartWidget.jsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useContext } from "react";
 
 export const CartWidget = () => {
+  const { cartQuantity } = useContext(CartContext);
   return (
-    <>
-      <i className="bi bi-cart3"></i>
-    </>
+    <div className="d-flex justify-content-around align-items-center">
+      <BsCart4 fontSize={"1.5rem"} color="black" />
+      {cartQuantity() > 0 && <Badge bg="danger">{cartQuantity()}</Badge>}
+    </div>
   );
 };
 export default CartWidget;
