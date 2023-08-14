f3f5676760c8802a958ee2cce562784e6fd22665
florencia <florenciazuazaga47@gmail.com>
2023-08-14 13:39:09 -0300

    cambiodetalle

--- src/componentes/CartWidget/ItemCount/ItemDetailContainer/ItemDetail/index.jsx
+++ src/componentes/CartWidget/ItemCount/ItemDetailContainer/ItemDetail/index.jsx
@@ -13,7 +13,7 @@ export const ItemDetail = ({ data }) => {
   return (
     <div className="container">
       <div className="datail">
-        <img className="datail_image" src="{data.image}" alt="" />
+        <img className="datail_image" src={data.image} alt="" />
         <div className="content">
           <h1>{data.titulo}</h1>
           {goToCart ? (
