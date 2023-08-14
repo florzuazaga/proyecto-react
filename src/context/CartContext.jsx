a763015a502e7f6c25c332c3b4c195b04ee5df9e
florencia <florenciazuazaga47@gmail.com>
2023-08-14 14:53:17 -0300

    cambiocontext

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -6,7 +6,7 @@ export const useCartContext = () => useContext(CartContext);
 const CartProvider = ({ children }) => {
   const [cart, setCart] = useState([]);
   const addProduct = (item, newQuantity) => {
-    const newCart = cart.filter(prod.id !== item.id);
+    const newCart = cart.filter((prod) => prod.id !== item.id);
     newCart.push({ ...item, quantity: newQuantity });
     setCart(newCart);
   };
@@ -21,7 +21,8 @@ const CartProvider = ({ children }) => {
   };
 
   const clearCart = () => setCart([]);
-  const isInCart = (id) => (cart.find(product.id === id) ? true : false);
+  const isInCart = (id) =>
+    cart.find((product) => product.id === id) ? true : false;
   const removeProduct = (id) =>
     setCart(cart.filter((product) => product.id !== id));
 
